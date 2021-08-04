6e398e267d86e69e7e802aebd2267f7b3ce0584f
Ekrem Sağlam <ekremsaglammm@gmail.com>
2021-08-04 12:10:48 +0300

    Final Commit

--- Program.cs
+++ Program.cs
@@ -6,7 +6,49 @@ namespace arrayMethods
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-        }
+            //sort
+            int[] numberArray = { 23, 12, 4, 86, 72, 3, 11, 17 };
+            Console.WriteLine("Unsorted List");
+            foreach (int item in numberArray)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine("\nSorted List");
+            Array.Sort(numberArray);
+            foreach (int item in numberArray)
+            {
+                Console.Write(item + " ");
+            }
+
+            //clear
+            Console.WriteLine("\nCleaned List");
+            Array.Clear(numberArray, 2, 3);
+            foreach (int item in numberArray)
+            {
+                Console.Write(item + " ");
+            }
+
+            //reverse
+            Console.WriteLine("\nReversed List");
+            Array.Reverse(numberArray);
+            foreach (int item in numberArray)
+            {
+                Console.Write(item + " ");
+            }
+
+            //indexOf
+            Console.WriteLine("\nArray IndexOf");
+            int indexOfNumber = Array.IndexOf(numberArray,23);
+            Console.WriteLine("Sayının indexi : {0}",indexOfNumber);
+
+            //Resize
+            Console.WriteLine("\nArray Resize");
+            Array.Resize<int>(ref numberArray,15);
+            foreach (int item in numberArray)
+            {
+                Console.Write(item + " ");
+            }           
+
+        }//main end
     }
 }
