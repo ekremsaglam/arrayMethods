3abc00d0fc7328a753c0567a63d12e4eec008a5d
Ekrem Sağlam <ekremsaglammm@gmail.com>
2021-08-04 11:58:04 +0300

    First Commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace arrayMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
